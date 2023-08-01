1f80a59380ea76d13d42b7f21b7b62dd4337d7ec
eng-oday <odaydieg@gmail.com>
2023-08-01 17:04:36 +0300

    prepare url request and send it to get  response data

--- PhotoApp/SignUp/Netowrk/SignUpWebService.swift
+++ PhotoApp/SignUp/Netowrk/SignUpWebService.swift
@@ -6,17 +6,36 @@
 //
 
 import Foundation
-
+import UIKit
 
 class SignUpWebService {
     
+    private var urlSession:URLSession
     private var urlString:String
     
-    init(urlString: String) {
+    init(urlString: String,urlSession:URLSession = .shared) {
         self.urlString = urlString
+        self.urlSession = urlSession
     }
     
     func SignUp(withForm SignUpModel:SignUpFormRequestModel , completionHandler:@escaping (SignUpResponseModel?, SignUpError?) -> Void) {
         
+        // 1. SEETUP URL
+        guard let url = URL(string: urlString) else {
+            // TODO : make unit test to test that specific error message is returned is url is nil
+            return
+        }
+        // 2. PREPARE URL REQUEST
+        var urlRequest          = URLRequest(url: url)
+        urlRequest.httpMethod   = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+        urlRequest.httpBody  = try? JSONEncoder().encode(SignUpModel)
+        
+        // 3. CREATE DATA TASK
+        let dataTask        =  urlSession.dataTask(with: urlRequest) { data, response, error in
+            
+        }
+        dataTask.resume()
     }
 }
