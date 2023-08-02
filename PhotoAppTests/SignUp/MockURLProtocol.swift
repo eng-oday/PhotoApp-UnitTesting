653b976654a786415ac0eb569ff50c0e2b529ff1
eng-oday <odaydieg@gmail.com>
2023-08-02 14:54:56 +0300

    build mock url protocol

---
+++ PhotoAppTests/SignUp/MockURLProtocol.swift
@@ -0,0 +1,33 @@
+//
+//  MockURLProtocol.swift
+//  PhotoAppTests
+//
+//  Created by 3rabApp-oday on 02/08/2023.
+//
+
+import Foundation
+
+
+
+class MockUrlProtocol:URLProtocol {
+    
+    static var stubResponseData:Data?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        self.client?.urlProtocol(self, didLoad: MockUrlProtocol.stubResponseData ?? Data())
+        
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {
+        
+    }
+}
