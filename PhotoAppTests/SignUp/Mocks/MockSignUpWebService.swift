d59666f82bcaa2bc0e27541bc7da344679d5981c
eng-oday <odaydieg@gmail.com>
2023-08-06 13:30:59 +0300

    build unit test to verify SignUp was called

---
+++ PhotoAppTests/SignUp/Mocks/MockSignUpWebService.swift
@@ -0,0 +1,14 @@
+//
+//  MockSignUpWebService.swift
+//  PhotoAppTests
+//
+//  Created by 3rabApp-oday on 06/08/2023.
+//
+
+import Foundation
+
+
+class MockSignUpWebService {
+    
+    var isSignUpMethodCalled:Bool = false
+}
--- PhotoAppTests/SignUp/Presenters/SignUpPresenterTests.swift
+++ PhotoAppTests/SignUp/Presenters/SignUpPresenterTests.swift
@@ -30,10 +30,24 @@ final class SignUpPresenterTests: XCTestCase {
         sut.processUserSignUp(formModel:signUpFormModel)
         
         //ASSERT
-        XCTAssertTrue(mockSignUpModelValidator.isFirstNameValidate)
-        XCTAssertTrue(mockSignUpModelValidator.isValidLastNme)
-        XCTAssertTrue(mockSignUpModelValidator.isFirstNameValidate)
-        XCTAssertTrue(mockSignUpModelValidator.isFirstNameValidate)
+        XCTAssertTrue(mockSignUpModelValidator.isFirstNameValidate, "first name was not validated")
+        XCTAssertTrue(mockSignUpModelValidator.isValidLastNme, "last name wase not validated")
+        XCTAssertTrue(mockSignUpModelValidator.isValidPassword , "passwrod is not validated")
+        XCTAssertTrue(mockSignUpModelValidator.IsPasswordMatched , "password is not matched")
         
+    }
+    
+    func testSignUpPresenter_WhenGivenValidFormModel_ShouldCallSignUpMethod(){
+        
+        //ARRANGE
+        let signUpFormModel = SignUpFormModel(firstName:"oday" , lastName:"Mohammed" ,email:"oday.mo@gmail.com",password:"123456" , repeatPassword:"123456")
+        let mockSignUpModelValidator = MockSignUpModelValidator()
+        let mockSignUpWebService    = MockSignUpWebService()
+        let sut = SignUpPresenter(signUpFormValidator:mockSignUpModelValidator)
+        //ACT
+        sut.processUserSignUp(formModel:signUpFormModel)
+        //ASSERT
+        
+        XCTAssertTrue(mockSignUpWebService.isSignUpMethodCalled , "the SignUp()method was not called in SignUpWebService class ")
     }
 }
