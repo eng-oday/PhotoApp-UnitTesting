9916916be12cd8b00575cb7fb8cac40d7c60da3c
eng-oday <odaydieg@gmail.com>
2023-08-07 16:37:08 +0300

    test textField outlet has connected with StoryBoard using XCTUnwrap

--- PhotoAppTests/SignUp/SignUpViewControlerTests.swift
+++ PhotoAppTests/SignUp/SignUpViewControlerTests.swift
@@ -24,18 +24,26 @@ final class SignUpViewControlerTests: XCTestCase {
     }
     
     
-    func testSignUpViewController_WhenCreated_ShouldHaveEmptyFields(){
+    func testSignUpViewController_WhenCreated_ShouldHaveEmptyFields() throws{
         
         //Arrange
         // .. ON SETUP FUNCTION
         //Act
         
         //Assert
-        XCTAssertEqual(sut?.firstNameTextField.text, "")
-        XCTAssertEqual(sut?.lastNameTextField.text, "")
-        XCTAssertEqual(sut?.emailTextField.text, "")
-        XCTAssertEqual(sut?.passwordTextField.text, "")
-        XCTAssertEqual(sut?.repeatPasswordTextField.text, "")
+        // TO UNWRAP OUTLET AND GET ERROR WHEN IT BE NIL ,
+        let firstNameTextField = try XCTUnwrap(sut?.firstNameTextField , "the first name outlet not connected to your vc ")
+        let lastNameTextField = try XCTUnwrap(sut?.lastNameTextField , "the last name outlet not connected to your vc ")
+        let emailTextField = try XCTUnwrap(sut?.emailTextField , "the email outlet not connected to your vc ")
+        let passwordTextField = try XCTUnwrap(sut?.passwordTextField , "the password outlet not connected to your vc ")
+        let RepeatPasswordTextField = try XCTUnwrap(sut?.repeatPasswordTextField , "the repeat password outlet not connected to your vc ")
+
+        
+        XCTAssertEqual(firstNameTextField.text, "", "the first name text field was not when the view cotroller initially loaded ")
+        XCTAssertEqual(lastNameTextField.text, "","the last name text field was not when the view cotroller initially loaded ")
+        XCTAssertEqual(emailTextField.text, "" , "the email text field was not when the view cotroller initially loaded ")
+        XCTAssertEqual(passwordTextField.text, "" , "the password text field was not when the view cotroller initially loaded ")
+        XCTAssertEqual(RepeatPasswordTextField.text, "" , "the repeat passsword text field was not when the view cotroller initially loaded ")
     }
 
 }
